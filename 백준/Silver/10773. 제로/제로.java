66cd21c264de1af6819dcbc24c47822c25d0150d
haram <dlfnadl97@naver.com>
2025-10-27 15:12:07 +0900

    [Silver IV] Title: 제로, Time: 176 ms, Memory: 20976 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 1<=K<=100_000
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0 ; i<K ; i++){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0){
+                stack.pop();
+                continue;
+            }
+            stack.push(n);
+        }
+        
+        int sum = 0;
+        
+        while(!stack.isEmpty()){
+            sum += stack.pop();
+        }
+        
+        System.out.println(sum);
+    }
+}
