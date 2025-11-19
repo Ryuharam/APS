0cb0edcc958924e63c130c45663c0a406b17a18f
haram <dlfnadl97@naver.com>
2025-11-19 15:21:56 +0900

    [Silver II] Title: 최대 힙, Time: 256 ms, Memory: 25212 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0 ; i<N ; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num==0){
+                sb.append(pq.isEmpty()? 0 : pq.poll()).append("\n");
+            }else{
+                pq.add(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
