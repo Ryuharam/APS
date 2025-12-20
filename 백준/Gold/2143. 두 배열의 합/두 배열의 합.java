93ac8f5ff6b811644ed8de06c75c505f55044403
haram <dlfnadl97@naver.com>
2025-12-20 17:08:40 +0900

    [Gold III] Title: 두 배열의 합, Time: 492 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -3,74 +3,81 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long T = Integer.parseInt(br.readLine());
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        long T = Long.parseLong(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        long[] A = new long[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            A[i] = Long.parseLong(st.nextToken());
         }
 
-        int M = Integer.parseInt(br.readLine());
-        int[] B = new int[M];
+        int m = Integer.parseInt(br.readLine());
+        long[] B = new long[m];
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < M; i++){
-            B[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            B[i] = Long.parseLong(st.nextToken());
         }
 
-        long[] sumA = getSubSum(A);
-        long[] sumB = getSubSum(B);
+        long[] sumA = getSumList(A);
+        long[] sumB = getSumList(B);
 
         Arrays.sort(sumA);
         Arrays.sort(sumB);
 
-        int l = 0;
-        int r = sumB.length - 1;
         long ans = 0;
+        int L = 0;
+        int R = sumB.length - 1;
+        while(L < sumA.length && R >= 0) {
+            long sum = sumA[L] + sumB[R];
 
-        while(l<sumA.length && r>=0){
-            long sa = sumA[l];
-            long sb = sumB[r];
-
-            if(sa + sb == T){
-                long tmpA = 0;
-                while(l<sumA.length && sumA[l] == sa){
-                    tmpA++;
-                    l++;
+            if(sum == T){
+                long cntA = 0;
+                int idx = L;
+                while(idx < sumA.length && sumA[L] == sumA[idx]) {
+                    cntA++;
+                    idx++;
                 }
+                L = idx;
 
-                long tmpB = 0;
-                while(r >= 0 && sumB[r] == sb){
-                    tmpB++;
-                    r--;
+                long cntB = 0;
+                idx = R;
+                while(idx >= 0 && sumB[R] == sumB[idx]) {
+                    cntB++;
+                    idx--;
                 }
+                R = idx;
 
-                ans += tmpA * tmpB;
-            }else if(sa + sb < T){
-                l++;
+                ans += (cntA * cntB);
+
+            }else if(sum > T){
+                R--;
             }else{
-                r--;
+                L++;
             }
         }
 
         System.out.println(ans);
     }
 
-    static long[] getSubSum(int[] array){
-        int n = array.length;
-        int size = n*(n+1)/2;
-        long[] result = new long[size];
+    public static long[] getSumList(long[] A) {
+        int len = A.length;
+        int size = len * ( len + 1 ) / 2;
+        long[] res = new long[size];
+
         int idx = 0;
-        for(int i=0 ; i<n; i++){
-            int sum = 0;
-            for(int j=i ; j<n ; j++){
-                sum += array[j];
-                result[idx++] = sum;
+        for(int i = 0 ; i<len ; i++){
+            long sum = 0;
+            for(int j = i ; j < len ; j++){
+                sum += A[j];
+                res[idx++] = sum;
             }
         }
-        return result;
+        return res;
     }
 
 }
