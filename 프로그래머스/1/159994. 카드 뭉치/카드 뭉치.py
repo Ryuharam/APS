2c08335afd014b617885be8bc9ca7487bce49ee5
haram <dlfnadl97@naver.com>
2026-08-14 08:52:44 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goal):
+    answer = 'Yes'
+    idx1, idx2 = 0, 0
+    
+    for i in range(len(goal)):
+        if idx1 < len(cards1) and cards1[idx1] == goal[i]:
+            idx1 += 1
+        elif idx2 < len(cards2) and cards2[idx2] == goal[i]:
+            idx2 += 1
+        else:
+            return 'No'
+    
+    return answer
